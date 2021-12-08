214912c322f38ead249c906f61ce941cbc85e49b
RealLukeMartin <lukemartinwebdev@gmail.com>
2021-12-08 17:07:57 -0600

    chore: test deploy update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is the Baseball League Builder API!';
+    return 'This is the Baseball League Builder API V2!';
   }
 }

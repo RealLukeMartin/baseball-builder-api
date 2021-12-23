f8ab8351057fbd443d7aaa87f038280f7e3a546d
RealLukeMartin <lukemartinwebdev@gmail.com>
2021-12-22 21:12:25 -0600

    chore: update ci and tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "This is the Baseball League Builder API!"', () => {
+      expect(appController.getHello()).toBe(
+        'This is the Baseball League Builder API!',
+      );
     });
   });
 });
